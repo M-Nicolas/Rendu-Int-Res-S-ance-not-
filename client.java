4f1f5fe3dbf6bf2a5aeff1177b7c0f9cd7e0b4d8
Corentin <corentin.fouyaya@hotmail.fr>
2015-11-16 15:19:00 +0100

    2

--- client.java
+++ client.java
@@ -2,5 +2,37 @@
 public class Client {
     public static void main(String[] args) {
         Socket socket = null;
+
+        DataOutputStream os = null; // output stream
+        BufferedReader is = null; // input stream
+
+        try {
+            socket = new Socket("", 1313);
+            System.out.println("debut client");
+
+            os = new DataOutputStream(socket.getOutputStream());
+            is = new BufferedReader(socket.getInputStream());
+
+            
+
+            /*
+            String tavu = checkAnswer(a);
+
+            if ("ok".equals(tavu)) {
+                showAction(a);
+                showValues(a);
+            } else if ("nope". equals(tavu)) {
+                System.out.println("Le serveur est pas gentil, il a refusé notre requète...");
+            } else {
+                System.out.println("Dear, we're in trouble !");
+            }
+            */
+
+            os.close();
+            is.close();
+            socket.close();
+        } catch (Exception e) {
+            System.err.println("Youston, we have a problem ! Voici le problème en question : " + e);
+        }
     }
 }
